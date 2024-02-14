89ced52e3c92ebaf9fa502aacd832062783e29e8
CristianMartinez1398 <109698231+CristianMartinez1398@users.noreply.github.com>
2024-02-14 01:52:18 -0600

    diseño

--- pulhapanzak-app/src/app/home/home.page.ts
+++ pulhapanzak-app/src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonNav, IonButton, IonIcon, IonInput, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonInput, IonLabel],
 })
 export class HomePage {
   constructor() {}
