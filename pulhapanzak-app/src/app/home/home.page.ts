9249c3f3bb9270c0b0dd422038597f4d6387b9fd
CristianMartinez1398 <109698231+CristianMartinez1398@users.noreply.github.com>
2024-02-21 01:32:28 -0600

    input
    se pusieron que fuera requeridos en todos los input y poner los minlegth en los input de DNI y teléfono que solo se acepten números.

--- pulhapanzak-app/src/app/home/home.page.ts
+++ pulhapanzak-app/src/app/home/home.page.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
-
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonNav, IonButton, IonInput, IonLabel } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonNav,
+  IonButton,
+  IonInput,
+  IonLabel,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput, IonLabel],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonInput,
+    IonLabel,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  formBuilder = inject(FormBuilder);
+
+  loginForm: FormGroup = this.formBuilder.group({
+    nombre: ['', Validators.required],
+    apellidos: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    contrasena: ['', Validators.required],
+    dni: ['', [Validators.required, Validators.minLength(13), Validators.pattern('^[0-9]+$')]],
+    numero: ['', [Validators.required, Validators.minLength(8), Validators.pattern('^[0-9]+$')]]
+  })
 }
